b5722e1d0a64ae5b67d00fcc3876f85986b4f5a5
Aubane COULOMBIER <acoulomb@student.42.fr>
2018-11-13 19:40:45 +0100

    filtres front wip

---
+++ models/resultSort.js
@@ -0,0 +1,34 @@
+class Sort{
+    constructor(){
+        this.errors = []
+    }
+
+    async sortBy(json, params){
+        try {
+            return new Promise((resolve, reject) => {
+				var dob = params;
+				console.log('dob',dob);
+				if (dob != null) {
+				    var year = dob.getFullYear();
+					var month = dob.getMonth();
+					var day = dob.getDate();
+
+					var today = new Date();
+					var age = today.getFullYear() - year;
+						if (today.getMonth() < month || (today.getMonth() === month && today.getDate() < day)) {
+						    age--;
+                        }
+                    resolve(age);
+                } else {
+					reject(null);
+				}
+               });
+        } catch (error){
+            console.log(error);
+            return false;
+        }
+    }
+
+}
+
+module.exports = Sort;
